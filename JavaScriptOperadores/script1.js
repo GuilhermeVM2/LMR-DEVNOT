da6c4b5164b152d383581d872b6f2756e1344247
GuilhermeVM2 <123783825+GuilhermeVM2@users.noreply.github.com>
2023-05-04 21:52:43 -0300

    Add files via upload

---
+++ JavaScriptOperadores/script1.js
@@ -0,0 +1,11 @@
+let numero1;
+let numero2;
+let resposta;
+
+numero1 = prompt('Digite um valor: ','Digite um Número');
+numero2 = prompt('Digite outro valor:','Digite um número');
+
+resposta = parseInt(numero1)+parseInt(numero2);
+
+document.write('O resultado da soma é ', resposta);
+
