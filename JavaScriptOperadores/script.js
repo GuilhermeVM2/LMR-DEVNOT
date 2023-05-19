d640cf2522592d861b47f8d92e46d850a360c5c3
GuilhermeVM2 <123783825+GuilhermeVM2@users.noreply.github.com>
2023-05-18 21:37:52 -0300

    Add files via upload

--- JavaScriptOperadores/script.js
+++ JavaScriptOperadores/script.js
@@ -1,3 +1,50 @@
 let NOME;
     NOME = prompt('Entre com seu nome','Digite-o aqui')
     document.write('Oi '+NOME+' esteja a vontade')
+
+let valor1;
+let valor2;
+let resultado;
+function Somar() {
+    valor1=document.getElementById("MyCalc").pvalor1.value;
+    valor2=document.getElementById("MyCalc").pvalor2.value;
+    if (valor1==""||valor2=="") {
+        alert("Digite um número")
+    }
+    else
+    resultado = parseInt(valor1)+parseInt(valor2);
+    alert("O resultado da soma é "+resultado);
+}
+function Sub() {
+    valor1=document.getElementById("MyCalc").pvalor1.value;
+    valor2=document.getElementById("MyCalc").pvalor2.value;
+    if (valor1==""||valor2=="") {
+        alert("Digite um número")
+    }
+    else
+    resultado = parseInt(valor1)-parseInt(valor2);
+    alert("O resultado da subtração é "+resultado);
+}
+function Mult() {
+    valor1=document.getElementById("MyCalc").pvalor1.value;
+    valor2=document.getElementById("MyCalc").pvalor2.value;
+    if (valor1==""||valor2=="") {
+        alert("Digite um número")
+    }
+    else
+    resultado = parseInt(valor1)+parseInt(valor2);
+    alert("O resultado da soma é "/resultado);
+}
+function Div() {
+    valor1=document.getElementById("MyCalc").pvalor1.value;
+    valor2=document.getElementById("MyCalc").pvalor2.value;
+    if (valor2==0) {
+        alert("Não é possível dividir por 0")
+    }
+    if (valor1==""||valor2==""||valor2==0) {
+        alert("Digite um número")
+    }
+    else
+    resultado = parseInt(valor1)+parseInt(valor2);
+    alert("O resultado da soma é "/resultado);
+}
